321c70f5f02cb8c58d4da5dc68d5a69b65d9a433
David Thien <dthien@eng.ucsd.edu>
2020-02-10 17:00:55 -0800

    Started work on semantics for instr_class

--- isabelle/Instr_Class.thy
+++ isabelle/Instr_Class.thy
@@ -1,18 +1,22 @@
 theory Instr_Class
-  imports reassembly_datatypes.MachineDatatypes
+  imports reassembly_datatypes.MachineDatatypes BitVectors Machine
 begin
   
 datatype instr_class = 
   Mem_Read register
-  | Mem_Write register address
-  | Bound_Checks register
+  | Mem_Write register bv
+  | Bound_Checks bv
   | Branch "address list"
-  | Reg_Write register address
+  | Reg_Write register bv
   | Stack_Expand int
   | Stack_Contract int
   | Stack_Read int
-  | Stack_Write int address
+  | Stack_Write int bv
   | Ret
 
+fun run_instr :: "instr_class \<Rightarrow> state \<Rightarrow> state"
+  where
+    "run_instr (Mem_Read (General bm r)) \<sigma> = Memory bit_mask address"
+  | "run_instr (Reg_Write (General bm r) bv) \<sigma> = write_reg (General bm r) ((<63,0>fst bv)::64 word) \<sigma>"
 
 end
