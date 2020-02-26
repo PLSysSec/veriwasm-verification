dcc48c61e2eb1165e182a7a878606bc290ad4685
Yousef Alhessi <yalhessi@gmail.com>
2020-02-25 17:31:00 -0800

    added some missing preconditions in the small step semantics and got started on big step semantics

--- Machine.v
+++ Machine.v
@@ -1,4 +1,4 @@
-Require Import Coq.FSets.FMapList.
+(*Require Import Coq.FSets.FMaps.*)
 Require Import Coq.Lists.List.
 (*Require Import Coq.Vectors.Vector.*)
 (*Require Import SFI.Machine.Bits.*)
@@ -45,13 +45,14 @@ Inductive info : Set :=
 | cf_bounded
 | heap_base.
 
+
 Definition fmap (A B:Type) := 
   A -> B.
 
-Definition set A (eq_dec:forall (x y:A),{x=y}+{x<>y}) B (f:fmap A B) (x:A) (v:B) : fmap A B :=
+Definition set {A} (eq_dec:forall (x y:A),{x=y}+{x<>y}) {B} (f:fmap A B) (x:A) (v:B) : fmap A B :=
   fun y => if eq_dec x y then v else f y.
 
-Definition get A B (f:fmap A B) (x:A) : B := 
+Definition get {A} {B} (f:fmap A B) (x:A) : B := 
   f x.
 
 Definition registers_ty := fmap register info.
@@ -167,10 +168,10 @@ Qed.
 *)
 
 Definition get_register_info (s : state) (r : register) : info :=
-  get register info s.(regs) r.
+  get s.(regs) r.
 
 Definition set_register_info (s : state) (r : register) (i : info) : state :=
-{| regs := set register register_eq_dec info s.(regs) r i;
+{| regs := set register_eq_dec s.(regs) r i;
    frame := s.(frame);
    stack := s.(stack);
 (*   heap := s.(heap) *) |}.
@@ -204,6 +205,9 @@ Definition set_stack_info (s : state) (index : nat) (i : info) : state :=
    stack := s.(stack);
  (*  heap := s.(heap) *) |}.
 
+Definition empty {A} (l : list A) :=
+  l = nil.
+
 Inductive instr_class := 
 | Heap_Read : register -> register -> register -> instr_class
 | Heap_Write : register -> register -> register -> instr_class
@@ -216,17 +220,20 @@ Inductive instr_class :=
 | Stack_Contract : nat -> instr_class
 | Stack_Read : register -> nat -> instr_class
 | Stack_Write : nat -> register -> instr_class
-| Ret : instr_class
-| Call : instr_class.
+| Indirect_Call : register -> instr_class
+| Direct_Call : instr_class
+| Ret : instr_class.
 
 Reserved Notation " st '|-' i '⟶' st' "
                   (at level 40, st' at level 39).
 Inductive instr_class_step : instr_class -> state -> state -> Prop := 
 | I_Heap_Read: forall st r_base r_src r_dst,
+    (* r_base <> r_src -> *) (* not sure if we need this to make the proofs easier *)
     get_register_info st r_base = heap_base ->
     get_register_info st r_src = mem_bounded ->
     st |- (Heap_Read r_dst r_src r_base) ⟶ (set_register_info st r_dst unbounded) 
 | I_Heap_Write: forall st r_base r_src r_dst,
+    (* r_base <> r_src -> *) (* not sure if we need this to make the proofs easier *)
     get_register_info st r_base = heap_base ->
     get_register_info st r_src = mem_bounded -> 
     st |- (Heap_Write r_dst r_src r_base) ⟶ st
@@ -243,22 +250,67 @@ Inductive instr_class_step : instr_class -> state -> state -> Prop :=
 | I_Stack_Contract: forall st i,
     st |- (Stack_Contract i) ⟶ (contract_stack st i)
 | I_Stack_Read: forall st i r_dst,
+    i < (length st.(frame)) ->
     st |- (Stack_Read r_dst i) ⟶ (set_register_info st r_dst (get_stack_info st i))
 | I_Stack_Write: forall st i r_src,
+    i < (length st.(frame)) ->
     st |- (Stack_Write i r_src) ⟶ (set_stack_info st i (get_register_info st r_src))
-| I_Ret: forall st,
-    st |- (Ret) ⟶ (pop_frame st)
-| I_Call: forall st,
+| I_Indirect_Call: forall st reg,
+    get_register_info st reg = cf_bounded ->
     get_register_info st rdi = heap_base ->
-    st |- (Call) ⟶ (push_frame st)
+    st |- (Indirect_Call reg) ⟶ (push_frame st)
+| I_Direct_Call: forall st,
+    get_register_info st rdi = heap_base ->
+    st |- (Direct_Call) ⟶ (push_frame st)
+| I_Ret: forall st,
+    empty st.(frame) ->
+    st |- (Ret) ⟶ (pop_frame st)
   where " st '|-' i '⟶' st' " := (instr_class_step i st st').
+
+Reserved Notation " st '|-' is '\longrightarrow*' st' "
+                  (at level 40, st' at level 39).
+Inductive instr_class_multi : instr_class -> state -> state -> Prop := .
 
 Definition basic_block := list instr_class.
 
 Record cfg_ty := {
-  nodes : list cfg_node;
-  edges : list (cfg_node * cfg_node)
+  nodes : list basic_block;
+  edges : list (basic_block * basic_block)
 }.
+
+Lemma register_get_after_set_eq : forall (regs : registers_ty) reg v,
+    get (set register_eq_dec regs reg v) reg = v.
+Proof.
+  intros regs reg v. unfold get. unfold set. destruct (register_eq_dec reg reg).
+  - reflexivity.
+  - exfalso. apply n. reflexivity.
+Qed.
+
+Lemma register_get_after_set_neq : forall (regs : registers_ty) reg1 reg2 v,
+    reg1 <> reg2 ->
+    get (set register_eq_dec regs reg2 v) reg1 = get regs reg1.
+Proof.
+  intros regs reg1 reg2 v Heq. unfold get. unfold set. destruct (register_eq_dec reg2 reg1).
+  - exfalso. apply Heq. auto.
+  - reflexivity.
+Qed.
+
+Definition safe_stack_read (st : state) (r_base : register) (r_src : register) (r_dst : register) : Prop :=
+  r_base <> r_src /\ get_register_info st r_base = heap_base /\ get_register_info st r_src = mem_bounded.
+
+Lemma read_after_check_is_safe: forall r_base r_src r_dst st, exists st' st'',
+  r_base <> r_src ->
+  get_register_info st r_base = heap_base ->
+  st |- (Heap_Check r_src) ⟶ st' /\ st' |- (Heap_Read r_dst r_src r_base) ⟶ st''.
+Proof.
+  intros r_base r_src r_dst st. 
+  eexists. eexists. intros Hneq Hbase.
+  split.
+  - apply I_Heap_Check.
+  - apply I_Heap_Read.
+    + unfold get_register_info in *. unfold set_register_info. simpl. rewrite register_get_after_set_neq; auto.
+    + unfold get_register_info in *. unfold set_register_info. simpl. rewrite register_get_after_set_eq; auto.
+Qed.
 
 Theorem sfi_property: forall (cfg : cfg_ty) exists st,
   (* local properties -> *)
