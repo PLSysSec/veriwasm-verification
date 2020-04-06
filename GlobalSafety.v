945b6447ff0e158c97f3d6680267a641dac2c5bf
David Thien <dthien@eng.ucsd.edu>
2020-04-06 10:55:19 -0700

    Added some empty theorems in GlobalSafety

--- GlobalSafety.v
+++ GlobalSafety.v
@@ -9,3 +9,29 @@ Require Import VerifiedVerifier.BinaryLattice.
 Require Import VerifiedVerifier.AbstractAnalysis.
 Require Import VerifiedVerifier.Semantics.
 Require Import Lia.
+
+
+(* TODO: Don't really know how to encode this*)
+(*
+Definition function_safety (f : function_ty) :=
+*)
+
+Definition program_safety (p : program_ty) : Prop :=
+  forall fuel,
+    (run_program p fuel).(error) = false.
+
+Theorem program_maintains_error :
+  forall s,
+    s.(error) = true ->
+    forall p cfg n fuel,
+      (run_program' p cfg n s fuel).(error) = true.
+Proof.
+  admit.
+Admitted.
+
+Theorem verified_program :
+  forall p,
+    verify_program p = true -> program_safety p.
+Proof.
+  admit.
+Admitted.
